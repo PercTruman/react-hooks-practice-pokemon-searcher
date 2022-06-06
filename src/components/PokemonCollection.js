eec8a2ba468659d8ff8f256d1a39cd171494eb5b
PercTruman <drshawnhart@gmail.com>
2022-06-06 12:02:19 -0500

    got initial render of all pokemon cards

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,13 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokemonCards}) {
+  const individualCards = pokemonCards.map((pokemon)=>
+  <PokemonCard key={pokemon.id} name={pokemon.name} hp={pokemon.hp} frontSprite={pokemon.sprites.front} backSprite={pokemon.sprites.back}/>)
   return (
     <Card.Group itemsPerRow={6}>
       <h1>Hello From Pokemon Collection</h1>
+      {individualCards}
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemonCards, setPokemonCards]= useState([])
+  useEffect(()=>{
+    fetch('http://localhost:3001/pokemon')
+    .then((res)=>res.json())
+    .then(data=>setPokemonCards(data))
+  },[])
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
@@ -12,8 +18,8 @@ function PokemonPage() {
       <PokemonForm />
       <br />
       <Search />
-      <br />
-      <PokemonCollection />
+      <br /> 
+      <PokemonCollection pokemonCards={pokemonCards}/>
     </Container>
   );
 }
